87042703635f79d9226ae4378ca3f69c46c2c024
svdf18 <simonvandeurs@hotmail.com>
2024-05-14 10:30:15 +0200

    BXB-76
    Frontend setup done w. login and backend connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import './App.css'
 import Login from './security/Login';
+import { Route, Routes } from 'react-router-dom'
 
 // make routes here!
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-        <h1>App</h1>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    </>
   )
-
 }
 
 export default App
