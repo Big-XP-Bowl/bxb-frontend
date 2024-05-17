1ff825409c3e525c87c36c2d662d7740096b9106
svdf18 <simonvandeurs@hotmail.com>
2024-05-17 09:55:15 +0200

    BXB-51
    useReservation init

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,32 @@
 import { Card1 } from '../styles/Cards';
 import { Grid2 } from '../styles/Grids';
+import useReservations from '../hooks/useReservations';
 
 const Home = () => {
-  
+  const { reservations, isLoading } = useReservations();
+
+  console.log(reservations);
+
   return (
     <>
+    <h2>Reservationer</h2>
     <Grid2>
-        <Card1>
-        <p>YEYEY</p>
-        </Card1>
-        <Card1>
-        <p>YEYEY</p>
-        </Card1>  
-      </Grid2>
-    </>
+      {isLoading && <p>Loading...</p>}
+      {reservations.map((reservation) => {
+        return (
+          <Card1 key={reservation.id}>
+            <h2>{reservation.id}</h2>
+            <p>{reservation.startTime}</p>
+            <p>{reservation.partySize}</p>
+            <p>{reservation.customerName}</p>
+            <p>{reservation.customerPhone}</p>
+          </Card1>
+        );
+      })}
+    </Grid2>
     
+    </>
   );
-};
+}
 
 export default Home;
