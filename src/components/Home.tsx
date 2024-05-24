78cd14418467e22833ba731f64e491a477225a00
Tunafan <124045808+Tunafan@users.noreply.github.com>
2024-05-24 11:29:14 +0200

    BXB-108-wavy-hero colors and hero
    Waves are styled. Colors are picked:
    PrimaryColor,
    SecondaryColor,
    ThirdColor

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,12 @@ import { HeroCard, HeroVideoCard } from "../styles/Cards";
 import Activities from "./Activities";
 import About from "./About";
 import Products from "./Products";
+import {
+  WavesSection,
+  WaveWrapperTop,
+  WaveWrapperBottom,
+} from "../styles/Curves";
+import { WaveTop, WaveBottom } from "../styles/Waves";
 
 const Home = () => {
   return (
@@ -23,20 +29,52 @@ const Home = () => {
           width: "102%",
           left: "-1%",
           top: "48px",
+          background: "transparent",
         }}
       >
-        <h1>VELKOMMEN TIL FEIN BOWLINGHAL</h1>
+        <h1
+          style={{
+            fontSize: "3rem",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+            margin: 0,
+          }}
+        >
+          VELKOMMEN TIL FEIN BOWLINGHAL
+        </h1>
       </HeroCard>
-
-      <HeroCard id="about">
-        <About />
-      </HeroCard>
-      <HeroCard id="activities">
-        <Activities />
-      </HeroCard>
-      <HeroCard id="products">
-        <Products />
-      </HeroCard>
+      <WavesSection>
+        <WaveWrapperTop>
+          <WaveTop />
+        </WaveWrapperTop>
+        <HeroCard id="about">
+          <About />
+        </HeroCard>
+        <WaveWrapperBottom>
+          <WaveBottom />
+        </WaveWrapperBottom>
+      </WavesSection>
+      <WavesSection>
+        <WaveWrapperTop>
+          <WaveTop />
+        </WaveWrapperTop>
+        <HeroCard id="activities">
+          <Activities />
+        </HeroCard>
+        <WaveWrapperBottom>
+          <WaveBottom />
+        </WaveWrapperBottom>
+      </WavesSection>
+      <WavesSection>
+        <WaveWrapperTop>
+          <WaveTop />
+        </WaveWrapperTop>
+        <HeroCard id="products">
+          <Products />
+        </HeroCard>
+        <WaveWrapperBottom>
+          <WaveBottom />
+        </WaveWrapperBottom>
+      </WavesSection>
     </>
   );
 };
